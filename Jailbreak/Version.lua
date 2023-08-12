fd6713f475c3e63424a52e684b1dd3455cf4b46e
Triet2804 <56880221+HackerTheBestYTB@users.noreply.github.com>
2023-08-12 18:43:03 +0700

    Update Version.lua

--- Jailbreak/Version.lua
+++ Jailbreak/Version.lua
@@ -1,12 +1,19 @@
 local data = {
-    version = nil
+	version = nil
 }
 
 local latest = '3.7'
 function data.checkVersion()
-    if data.version ~= latest then
-       message("Old Version")
-    end
+	if data.version ~= latest then
+		local PromptLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerTheBestYTB/ScriptRoblox/test/PromptLibrary.lua"))()
+		PromptLib("TVNHUB | Jailbreak","Your current version is outdated ("..tostring(data.version)..")\nYou need to update to latest version ("..tostring(latest)..")",{
+			{Text = "Keep Playing",LayoutOrder = 0,Primary = false,Callback = function()
+			end},
+			{Text = "Leave",LayoutOrder = 1,Primary = true,Callback = function()
+				game:Shutdown()   
+			end}
+		},true)
+	end
 end
 
 return data
