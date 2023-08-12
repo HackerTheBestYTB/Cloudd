15f1d5c73bacd32b942b5c42c5f5d28efda4c638
Triet2804 <56880221+HackerTheBestYTB@users.noreply.github.com>
2023-08-12 17:32:12 +0700

    Update Version.lua

--- Jailbreak/Version.lua
+++ Jailbreak/Version.lua
@@ -1,1 +1,12 @@
+local data = {
+    version = nil
+}
 
+local latest = '3.7'
+function data.checkVersion()
+    if data.version ~= latest then
+       print("Old Version")
+    end
+end
+
+return data
