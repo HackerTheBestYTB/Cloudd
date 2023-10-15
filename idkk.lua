71f42eb96ae138c7f19eb2998c2b2e53056a9884
Triet2804 <56880221+HackerTheBestYTB@users.noreply.github.com>
2023-10-15 12:57:55 +0700

    Update idkk.lua

--- idkk.lua
+++ idkk.lua
@@ -1,3 +1,23 @@
-function checkHWID()
-print(1)
+local data = {
+	hwid = nil,
+	hwidc = nil
+}
+
+local tableofhwid = {
+	"0e847c84c4d9f5fa8f1c63ba5847aac3",
+	"121be33c04cff2adc742fc9c2e848c2",
+	"LOL"
+}
+function data.checkHWID()
+	for _, value in ipairs(tableofhwid) do
+		if value == hwid then
+			hwidc = true
+			break
+		end
+	end
+	if not hwidc then
+		hwidc = false
+	end
 end
+
+return data, hwidc
