1aad7ae947e871d8b5840fe1be1800e83a93bb53
Triet2804 <56880221+HackerTheBestYTB@users.noreply.github.com>
2024-08-05 09:12:09 +0700

    Update Version.lua

--- Jailbreak/Version.lua
+++ Jailbreak/Version.lua
@@ -2,7 +2,7 @@ local data = {
 	version = nil
 }
 
-local latest = "3.8"
+local latest = "4.0"
 function data.checkVersion()
 	if data.version ~= latest then
 		local PromptLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerTheBestYTB/ScriptRoblox/test/PromptLibrary.lua"))()
